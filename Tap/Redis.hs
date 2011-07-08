fa62991f9a6dae09369df869162bec98b76b5129
Dave Fayram <dfayram@gmail.com>
2011-07-08 10:59:31 -0700

    Train fixes in. Enumerator to channel done.

--- Tap/Redis.hs
+++ Tap/Redis.hs
@@ -1,42 +1,51 @@
-module Tap.Redis (AtomicMessageStore, intoHistory) where 
-       
-import qualified Database.Redis.Redis as Redis
+module Tap.Redis (AtomicMessageStore
+                 , MessageStore
+                 , RedisMessage
+                 , storeChannelsInto) where 
 
-import Data.Enumerator (Iteratee(..), Stream(..), Step(..), returnI, liftI, continue, yield)
-import qualified Data.Enumerator.Redis as R
+import Prelude hiding (take)
+import Database.Redis.Redis (Message(..), Redis(..), subscribe)
+import Data.Enumerator (Iteratee(..), Stream(..), Step(..), 
+                        returnI, liftI, continue, yield, ($$), 
+                        run_)
+import Data.Enumerator.Redis (enumSubscriptions)
 
-import qualified Data.Sequence as Seq
-import Data.Sequence ((><), Seq(..))
+import Data.Sequence ((><), Seq(..), fromList, take, empty)
+import Data.ByteString (ByteString)
 
 import Control.Monad (forM_)
+import Control.Concurrent (forkIO, ThreadId(..))
 import Control.Concurrent.MonadIO (liftIO, MonadIO)
-import Data.ByteString (ByteString)
-import Control.Concurrent.STM.TVar (TVar, newTVar,readTVar,writeTVar)
 import Control.Monad.STM (atomically)
+import Control.Concurrent.STM.TVar
 
-type RedisMessage = Redis.Message ByteString
+
+type RedisMessage = Message ByteString
 type MessageStore = Seq RedisMessage
 type AtomicMessageStore = TVar MessageStore
 
 max_buffer_size = 64
 
-enqMessage :: AtomicMessageStore -> [RedisMessage] -> IO ()
-enqMessage tv xs = do
+enqMessages :: AtomicMessageStore -> [RedisMessage] -> IO ()
+enqMessages tv xs =  
   atomically $ do 
     history <- readTVar tv
-    let concated = Seq.fromList xs >< history in
-      writeTVar tv (Seq.take max_buffer_size concated)
-enqMessage _ _ = return ()          
-    
+    let concated = fromList xs >< history in
+      writeTVar tv (take max_buffer_size concated)
+enqMessage _ _ = return ()    
 
-intoHistory :: TVar MessageStore -> Iteratee RedisMessage IO ()
+intoHistory :: AtomicMessageStore -> Iteratee RedisMessage IO ()
 intoHistory tvar = continue step where
 	step (Chunks []) = continue step
-	step (Chunks xs) = liftIO (enqMessage tvar xs) >> continue step
+	step (Chunks xs) = (liftIO (enqMessages tvar xs)) >> continue step
 	step EOF = yield () EOF
 
 
-  
-  
-  
+storeChannelsInto :: Redis -> [String] -> IO (AtomicMessageStore, ThreadId)
+storeChannelsInto redis channels = do
+  subscribe redis channels :: IO [Message ()] -- We ignore the result, we don't care, so don't
+                                              -- allocate any space.
+  newStore <- newTVarIO empty
+  spawned  <- forkIO $ run_ ( enumSubscriptions redis 1000 $$ intoHistory newStore )
+  return (newStore, spawned)
 
